a29af5e1e143b67af6dd56b080aba1ea6892d836
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-02-13 14:40:14 +0300

    task complete

--- src/ru/geekbrains/java2/ChatWindow.java
+++ src/ru/geekbrains/java2/ChatWindow.java
@@ -2,28 +2,48 @@ package ru.geekbrains.java2;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatWindow extends JFrame {
 
     ChatWindow(){
         setTitle("Chat window");
         setSize(600, 400);
-        setResizable(false);
+        setMinimumSize(new Dimension(400,300));
         setLocation(600,400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
         JTextField textField = new JTextField();
         JButton button = new JButton("Send");
 
         add(textArea, BorderLayout.CENTER);
 
-        JPanel panel = new JPanel(new GridLayout(1,4,1,0));
-        panel.add(textField);
-        panel.add(button);
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(textField, BorderLayout.CENTER);
+        panel.add(button, BorderLayout.LINE_END);
         add(panel, BorderLayout.SOUTH);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append(textField.getText() + "\n");
+                textField.setText("");
+            }
+        });
+
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append(textField.getText() + "\n");
+                textField.setText("");
+            }
+        });
 
         setVisible(true);
 
     }
+
 }
