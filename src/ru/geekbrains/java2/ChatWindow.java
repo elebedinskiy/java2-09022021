48631b522083eb23528d536198649c53515df4fa
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-02-14 22:50:27 +0300

    task complete

--- src/ru/geekbrains/java2/ChatWindow.java
+++ src/ru/geekbrains/java2/ChatWindow.java
@@ -18,12 +18,19 @@ public class ChatWindow extends JFrame {
         textArea.setEditable(false);
         JTextField textField = new JTextField();
         JButton button = new JButton("Send");
+        JButton button2 = new JButton("Clear");
+        JButton button3 = new JButton("Exit");
 
         add(textArea, BorderLayout.CENTER);
 
+        JPanel panelButton = new JPanel(new GridLayout(1,3,1,0));
+        panelButton.add(button);
+        panelButton.add(button2);
+        panelButton.add(button3);
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(textField, BorderLayout.CENTER);
-        panel.add(button, BorderLayout.LINE_END);
+        panel.add(panelButton, BorderLayout.LINE_END);
         add(panel, BorderLayout.SOUTH);
 
         button.addActionListener(new ActionListener() {
